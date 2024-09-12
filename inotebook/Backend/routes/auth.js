51e8532d6cbe52fa0ee75c35a2a1af4ed9dfa334
kashyapanjali <anjalikashyap9608@gmail.com>
2024-09-12 18:58:09 +0530

    post_Api

--- inotebook/Backend/routes/auth.js
+++ inotebook/Backend/routes/auth.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const User = require("../models/User");
-const { useInsertionEffect } = require("react");
+// const { useInsertionEffect } = require("react");
 const router = express.Router();
 
 //Create a User using : POST "api/auth/".Doesn't require auth
-router.get("/", (req, res) => {
+router.post("/", (req, res) => {
   console.log(req.body);
   const user = User(req.body);
   user.save();
